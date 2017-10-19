fe7326cf65bc18b16f5562558c4d3903f4f6196a
汤赐 <tangc@sunrizetech.com>
2017-10-19 15:49:52 +0800

    目录结构调整与完善

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 ## 森锐前端开发脚手架工具
-## 自动构建项目目录
+### 自动构建项目目录
+使用方法
+第一次使用：
+
+    npm install -g yo
+
+然后直接安装脚手架
+
+    npm install -g generator-srtech
+
+在合适的地方新建一个文件夹，在文件夹下运行：
+
+    yo srtech
+
+> 以后就可以直接命令行运行yo srtech，构建新项目
---
+++ generator-srtech/app/index.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var chalk = require('chalk');    //不同颜色的info
+var util = require('util');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');    //yeoman弹出框
+var path = require('path');
+var srtech = yeoman.Base.extend({
+    info: function() {
+        this.log(chalk.green(
+            '开始构建项目!'
+        ));
+    },
+
+    generateBasic: function() {  //按照自己的templates目录自定义
+        this.directory('src', 'src');    //拷贝目录
+        this.directory('build', 'build');
+        this.directory('common', 'common');
+        this.directory('config', 'config');
+        this.directory('dist', 'dist');
+        this.directory('static', 'static');
+        this.copy('package.json', 'package.json');   //拷贝文件
+        this.copy('index.html', 'index.html');
+        this.copy('README.md', 'README.md');
+        this.copy('webpack.config.js', 'webpack.config.js');
+    },
+    generateClient: function() {
+        this.sourceRoot(path.join(__dirname, 'templates'));
+        this.destinationPath('./');
+    },
+    install: function() {      //安装依赖
+        this.installDependencies({
+            skipInstall: this.options['skip-install']
+        });
+    },
+    end: function() {
+        this.log(yosay(
+            '构建成功!'
+        ));
+    }
+});
+module.exports = srtech;
